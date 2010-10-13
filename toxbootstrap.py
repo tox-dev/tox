38b80d532d5dd48afe10ca693936e96e3117da4e
srid <none@none>
2010-10-13 16:54:27 -0700

    bootstrap: update urls/refs in docstring

--- toxbootstrap.py
+++ toxbootstrap.py
@@ -3,7 +3,9 @@
 tox-bootstrap
 =============
 
-http://code.google.com/p/pytox/issues/detail?id=1
+A bootstrap to automatically install tox and dependencies on machines that may
+not already have tox installed. This is useful when configuring a number of
+Hudson quickly; only Python needs to be installed.
 
 Getting started
 ---------------
@@ -13,7 +15,7 @@ Getting started
     $ cd my_project/
     $ ls
     . .. src/ doc/ setup.py tox.ini
-    $ curl http://github.com/srid/tox-bootstrap/raw/master/toxbootstrap.py -O
+    $ curl http://pytox.googlecode.com/hg/toxbootstrap.py -O
 
 Instead of running "tox", now you can just run "python toxbootstrap.py" which
 will take care of installing tox (if not already installed into
