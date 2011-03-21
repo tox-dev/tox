deb0181a10b172c072a1d2ba96954af2cbfcd4fe
holger krekel <holger@merlinux.eu>
2011-03-21 16:12:57 +0100

    fix issue 20 - document development URLs for deps

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,5 +1,5 @@
 #
-__version__ = '0.9.1.dev1'
+__version__ = '0.9.1.dev2'
 
 from py.apipkg import initpkg
 
--- toxbootstrap.py
+++ toxbootstrap.py
@@ -61,7 +61,7 @@ ToDo
 
 """
 
-__version__ = '0.9.1.dev1'
+__version__ = '0.9.1.dev2'
 
 import sys
 import os
