ad9070293961d309bfc802e595ed782cbdec05fe
holger krekel <holger@merlinux.eu>
2011-05-28 14:55:59 +0200

    finalize tox 1.0

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,5 +1,5 @@
 #
-__version__ = '1.0.dev6'
+__version__ = '1.0'
 
 class exception:
     class Error(Exception):
--- toxbootstrap.py
+++ toxbootstrap.py
@@ -58,7 +58,7 @@ ToDo
 
 """
 
-__version__ = '1.0.dev6'
+__version__ = '1.0'
 
 import sys
 import os
