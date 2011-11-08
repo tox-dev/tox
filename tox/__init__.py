2316055b2a7bae9ff6ff9b8511b35f585e8c7f92
holger krekel <holger@merlinux.eu>
2011-11-08 22:03:51 +0000

    bump version to 1.2 for soon releasing it

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,5 +1,5 @@
 #
-__version__ = '1.2.dev5'
+__version__ = '1.2'
 
 class exception:
     class Error(Exception):
--- toxbootstrap.py
+++ toxbootstrap.py
@@ -58,7 +58,7 @@ ToDo
 
 """
 
-__version__ = '1.2.dev5'
+__version__ = '1.2'
 
 import sys
 import os
