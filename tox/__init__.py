3cdf9bd04178f306f77cfbfd750fe24f7ef0a689
holger krekel <holger@merlinux.eu>
2011-12-16 23:01:25 +0000

    bump version

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,5 +1,5 @@
 #
-__version__ = '1.3.dev1'
+__version__ = '1.3.dev2'
 
 class exception:
     class Error(Exception):
--- toxbootstrap.py
+++ toxbootstrap.py
@@ -58,7 +58,7 @@ ToDo
 
 """
 
-__version__ = '1.3.dev1'
+__version__ = '1.3.dev2'
 
 import sys
 import os
