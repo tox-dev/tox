58381961c8c878214386085484d4fcdaa88e0da8
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-05-29 23:20:24 +0200

    add python3.3 to the default env, so early adopters can test

--- tox/_config.py
+++ tox/_config.py
@@ -10,7 +10,7 @@ import py
 import tox
 
 defaultenvs = {'jython': 'jython', 'pypy': 'pypy'}
-for _name in "py24,py25,py26,py27,py30,py31,py32".split(","):
+for _name in "py24,py25,py26,py27,py30,py31,py32,py33".split(","):
     defaultenvs[_name] = "python%s.%s" %(_name[2], _name[3])
 
 def parseconfig(args=None, pkg=None):
