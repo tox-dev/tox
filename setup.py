6f6abddae3d6ff5051476691d6bbd5baec58bab9
holger krekel <holger@merlinux.eu>
2015-04-20 20:40:23 +0200

    remove empty vendor directory

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main():
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
         author='holger krekel',
         author_email='holger@merlinux.eu',
-        packages=['tox', 'tox.vendor'],
+        packages=['tox'],
         entry_points={'console_scripts': 'tox=tox:cmdline\ntox-quickstart=tox._quickstart:main'},
         # we use a public tox version to test, see tox.ini's testenv
         # "deps" definition for the required dependencies
