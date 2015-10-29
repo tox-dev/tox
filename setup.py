8a0ab7873293a322ddbb680bc027895d663d8a52
holger krekel <holger@merlinux.eu>
2015-10-29 14:55:56 +0100

    add changelog

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def main():
         description='virtualenv-based automation of test activities',
         long_description=open("README.rst").read(),
         url='http://tox.testrun.org/',
-        version='2.1.1',
+        version='2.1.2.dev1',
         license='http://opensource.org/licenses/MIT',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
         author='holger krekel',
--- tox/__init__.py
+++ tox/__init__.py
@@ -1,5 +1,5 @@
 #
-__version__ = '2.1.1'
+__version__ = '2.1.2.dev1'
 
 from .hookspecs import hookspec, hookimpl  # noqa
 
