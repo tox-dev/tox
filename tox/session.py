8d701f5a7e20b070b9ede19a61b528dbbd96fc0e
Sachi King <nakato@nakato.io>
2016-02-29 15:45:11 +1100

    Process minversion before all else
    Reading the tox.ini config file and processing it is done as one task, yet
    processing the minversion is done much later, as such if there is a bug fix
    in a newer version or a change that makes the tox.ini file incompatable with
    older versions tox will bail with a stack trace instead of a
    "Your tox is too old" message.
    
    This moves the version check as early as it can possibly be done, the very
    first value read out of tox.ini is minversion and it is immedeiatly compared
    to tox.__version__.

--- tox/session.py
+++ tox/session.py
@@ -40,6 +40,10 @@ def main(args=None):
         raise SystemExit(retcode)
     except KeyboardInterrupt:
         raise SystemExit(2)
+    except tox.exception.MinVersionError as e:
+        r = Reporter(None)
+        r.error(e.message)
+        raise SystemExit(1)
 
 
 def show_help(config):
@@ -233,6 +237,12 @@ class Reporter(object):
         self._reportedlines = []
         # self.cumulated_time = 0.0
 
+    def _get_verbosity(self):
+        if self.session:
+            return self.session.config.option.verbosity
+        else:
+            return 2
+
     def logpopen(self, popen, env):
         """ log information about the action.popen() created process. """
         cmd = " ".join(map(str, popen.args))
@@ -257,11 +267,11 @@ class Reporter(object):
         self.tw.sep("_", "summary")
 
     def info(self, msg):
-        if self.session.config.option.verbosity >= 2:
+        if self._get_verbosity() >= 2:
             self.logline(msg)
 
     def using(self, msg):
-        if self.session.config.option.verbosity >= 1:
+        if self._get_verbosity() >= 1:
             self.logline("using %s" % (msg,), bold=True)
 
     def keyboard_interrupt(self):
@@ -297,15 +307,15 @@ class Reporter(object):
         self.tw.line("%s" % msg, **opts)
 
     def verbosity0(self, msg, **opts):
-        if self.session.config.option.verbosity >= 0:
+        if self._get_verbosity() >= 0:
             self.logline("%s" % msg, **opts)
 
     def verbosity1(self, msg, **opts):
-        if self.session.config.option.verbosity >= 1:
+        if self._get_verbosity() >= 1:
             self.logline("%s" % msg, **opts)
 
     def verbosity2(self, msg, **opts):
-        if self.session.config.option.verbosity >= 2:
+        if self._get_verbosity() >= 2:
             self.logline("%s" % msg, **opts)
 
     # def log(self, msg):
@@ -359,14 +369,6 @@ class Session:
 
     def runcommand(self):
         self.report.using("tox-%s from %s" % (tox.__version__, tox.__file__))
-        if self.config.minversion:
-            minversion = NormalizedVersion(self.config.minversion)
-            toxversion = NormalizedVersion(tox.__version__)
-            if toxversion < minversion:
-                self.report.error(
-                    "tox version is %s, required is at least %s" % (
-                        toxversion, minversion))
-                raise SystemExit(1)
         if self.config.option.showconfig:
             self.showconfig()
         elif self.config.option.listenvs:
