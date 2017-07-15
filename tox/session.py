da05e971313ef0931a5d05af022f4f0d6a35dd4c
Vaskó László <vlaci@balabit.com>
2017-07-15 16:18:13 +0200

    session: report more meaningful errors on why virtualenv creation failed
    virtualenv fails if the destination directory contains unicode
    characters or spaces. A more descriptive error is printed if it's
    likely that that was the case.
    I didn't precheck the directory path for not supported characters
    because if this problem is fixed by upstream we wouldn't want
    execution to fail.

--- tox/session.py
+++ tox/session.py
@@ -449,8 +449,19 @@ class Session:
             envlog = self.resultlog.get_envlog(venv.name)
             try:
                 status = venv.update(action=action)
-            except tox.exception.InvocationError:
-                status = sys.exc_info()[1]
+            except FileNotFoundError as e:
+                status = (
+                    "Error creating virtualenv. "
+                    "Note that spaces in path are not supported by virtualenv. "
+                    "Error details: %r" % e
+                )
+            except tox.exception.InvocationError as e:
+                status = (
+                    "Error creating virtualenv. "
+                    "Note that some special characters (such as ':' and unicode symbols) "
+                    "in path are not supported by virtualenv. "
+                    "Error details: %r" % e
+                )
             if status:
                 commandlog = envlog.get_commandlog("setup")
                 commandlog.add_command(["setup virtualenv"], str(status), 1)
