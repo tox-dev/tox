be2a7729aef1f4055a5f4d72531b12a68d5f9080
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-09-18 17:40:26 +0200

    docs fixes
    * fragment inclusion in normal docs build on import of `conf.py` caused readthedocs builds to fail (https://readthedocs.org/projects/tox/builds/5956259/). Adapted code and moved it into pre-process script to be integrated in a more robust way later, if deemed necessary but for now it has to be enough to generate and integrate newsfragments only as part of a release.
    * fix name of element passed from towncrier for issue links (issue)
    * use toxworkdir for sphinx-build path
    * fix header levels in changelog to make changelog header level 1

--- tasks/pre-process-changelog.py
+++ tasks/pre-process-changelog.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3.6
+import os
 import re
+import subprocess
 import sys
 from pathlib import Path
 
 
 def main():
     manipulate_the_news()
+
+
+# todo integrate that into the test/build process somehow
+def include_draft_newsfragments():
+    """
+    generate and include into the changelog news fragments so they are also subject to the CI,
+    whenever a new release is done there should be no news fragment and as such this will have
+    no effect
+    """
+    project_root = Path(__file__).parents[1]
+    current_path = os.getcwd()
+    try:
+        os.chdir(project_root)
+        cmd = ['towncrier', '--draft', '--dir', project_root]
+        out = subprocess.check_output(cmd).decode('utf-8').strip()
+        docs_build_dir = project_root / '.tox' / 'docs' / 'fragments.rst'
+        docs_build_dir.write(out)
+    finally:
+        os.chdir(current_path)
 
 
 def manipulate_the_news():
