9ef7012628d4e1b984e71f8ed8eff722325c682a
Ederag <edera@gmx.fr>
2018-02-01 18:50:29 +0100

    more dense layout (request from gaborbernat)

--- tests/test_z_cmdline.py
+++ tests/test_z_cmdline.py
@@ -636,10 +636,8 @@ def _alwayscopy_not_supported():
     if hasattr(platform, 'linux_distribution'):
         _dist = platform.linux_distribution(full_distribution_name=False)
         (name, version, arch) = _dist
-        if any((
-                name == 'centos' and version[0] == '7',
-                name == 'SuSE' and arch == 'x86_64',
-               )):
+        if any(( name == 'centos' and version[0] == '7',
+                 name == 'SuSE' and arch == 'x86_64' )):
             return True
     return False
 
