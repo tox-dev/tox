a6c3ff614da458e5cb950d176473c34fcf53731d
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-04-28 09:56:55 +0200

    some fixmes/todos about the pytestplugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# FIXME modules containing fixtures should not be imported
+# FIXME this seems unnecessary
 # TODO move fixtures here and only keep helper functions/classes in the plugin
 # TODO _pytest_helpers might be a better name than _pytestplugin then?
 from tox._pytestplugin import *  # noqa
