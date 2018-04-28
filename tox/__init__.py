558000c4879e1531ebd1174d16859757ae9a852b
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-04-28 11:52:48 +0200

    API: add docstring and mark elements appropriately

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,6 +1,15 @@
-import pluggy
+"""Everything made explicitly available via `__all__` can be considered as part of the tox API.
+
+We will emit deprecation warnings for one minor release before making changes to these objects.
+
+If objects are marked experimental they might change between minor versions.
+
+To override/modify tox behaviour via plugins see `tox.hookspec` and its use with pluggy.
+"""
 from pkg_resources import DistributionNotFound
 from pkg_resources import get_distribution
+
+import pluggy
 
 from . import exception
 from .constants import INFO
@@ -9,18 +18,19 @@ from .constants import PYTHON
 from .hookspecs import hookspec
 
 __all__ = (
-    'cmdline', 'exception', '__version__',
-    'PYTHON', 'INFO', 'PIP',
-    'hookimpl',
-    'hookspec',  # DEPRECATED will be removed from API - see warning below
-)
-"""Everything explicitly exported here is part of the tox programmatic API.
+    '__version__',  # tox version
+    'cmdline',      # run tox as part of another program/IDE (same behaviour as called standalone)
+    'hookimpl',     # Hook implementation marker to be imported by plugins
+    'exception',    # tox specific exceptions
 
-To override/modify tox behaviour via plugins see tox.hookspec and its use with pluggy.
-"""
+    # EXPERIMENTAL CONSTANTS API
+    'PYTHON', 'INFO', 'PIP',
+
+    # DEPRECATED - will be removed from API in tox 4
+    'hookspec',
+)
 
 hookimpl = pluggy.HookimplMarker("tox")
-"""Hook implementation marker to be imported by plugins."""
 
 try:
     _full_version = get_distribution(__name__).version
