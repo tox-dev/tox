e5c94a5b5ef7ed2265db3052bc6b9bd6330c7db1
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-05-26 10:22:50 +0200

    missed that py executable basename is pypy when running with pypy

--- tox/_pytestplugin.py
+++ tox/_pytestplugin.py
@@ -39,6 +39,32 @@ def pytest_report_header():
     return "tox comes from: {!r}".format(tox.__file__)
 
 
+def pytest_collection_finish():
+    """Handle the pytest collection finish hook: configure pyannotate.
+    Explicitly delay importing `collect_types` until all tests have
+    been collected.  This gives gevent a chance to monkey patch the
+    world before importing pyannotate.
+    """
+    from pyannotate_runtime import collect_types
+
+    collect_types.init_types_collection()
+
+
+@pytest.fixture(autouse=True)
+def collect_types_fixture():
+    from pyannotate_runtime import collect_types
+
+    collect_types.resume()
+    yield
+    collect_types.pause()
+
+
+def pytest_sessionfinish():
+    from pyannotate_runtime import collect_types
+
+    collect_types.dump_stats("type_info.json")
+
+
 @pytest.fixture
 def work_in_clean_dir(tmpdir):
     with tmpdir.as_cwd():
