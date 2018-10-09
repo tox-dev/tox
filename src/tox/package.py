bf51012768b314de891629bff1e61f07a90785ca
Bernát Gábor <gaborjbernat@gmail.com>
2018-10-09 09:51:15 +0100

    session packages are now put inside a numbered directory (#1046)
    because pip fails when wheels are not named according to PEP-491
    https://www.python.org/dev/peps/pep-0491/#id9, and prefix
    numbering messes with this
    fallback to copy if link fails

--- src/tox/package.py
+++ src/tox/package.py
@@ -55,24 +55,30 @@ def create_session_view(package, temp_dir, report):
     """
     if not package:
         return package
-    temp_dir.ensure(dir=True)
+    package_dir = temp_dir.join("package")
+    package_dir.ensure(dir=True)
 
-    # we'll prefix it with a unique number, note adding as suffix can cause conflicts
-    # with tools that check the files extension (e.g. pip)
-    exists = [
-        i.basename[: -len(package.basename) - 1]
-        for i in temp_dir.listdir(fil="*-{}".format(package.basename))
-    ]
+    # we'll number the active instances, and use the max value as session folder for a new build
+    # note we cannot change package names as PEP-491 (wheel binary format)
+    # is strict about file name structure
+    exists = [i.basename for i in package_dir.listdir()]
     file_id = max(chain((0,), (int(i) for i in exists if six.text_type(i).isnumeric())))
-    session_package = temp_dir.join("{}-{}".format(file_id + 1, package.basename))
+
+    session_dir = package_dir.join(str(file_id + 1))
+    session_dir.ensure(dir=True)
+    session_package = session_dir.join(package.basename)
 
     # if we can do hard links do that, otherwise just copy
-    operation = "links"
+    links = False
     if hasattr(os, "link"):
-        os.link(str(package), str(session_package))
-    else:
-        operation = "copied"
+        try:
+            os.link(str(package), str(session_package))
+            links = True
+        except (OSError, NotImplementedError):
+            pass
+    if not links:
         package.copy(session_package)
+    operation = "links" if links else "copied"
     common = session_package.common(package)
     report.verbosity1(
         "package {} {} to {} ({})".format(
