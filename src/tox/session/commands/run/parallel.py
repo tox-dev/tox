2dd0f8b8a0d7eaa6666cb42f5234f2370fd3b778
Fabrice Flore-Thebault <themr0c@users.noreply.github.com>
2019-03-28 15:26:02 +0100

    add nospinner, second tentative
    Signed-off-by: Fabrice Flore-Thebault <themr0c@users.noreply.github.com>

--- src/tox/session/commands/run/parallel.py
+++ src/tox/session/commands/run/parallel.py
@@ -26,7 +26,7 @@ def run_parallel(config, venv_dict):
     semaphore = Semaphore(max_parallel)
     finished = Event()
 
-    show_progress = (not live_out or not nospinner) and reporter.verbosity() > reporter.Verbosity.QUIET
+    show_progress = not live_out and not nospinner and reporter.verbosity() > reporter.Verbosity.QUIET
     with Spinner(enabled=show_progress) as spinner:
 
         def run_in_thread(tox_env, os_env, processes):
