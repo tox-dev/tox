e36f770d47c627beed5431bc9511df40161d52f2
Bernat Gabor <bgabor8@bloomberg.net>
2019-04-16 15:36:43 +0100

    fix flaky test

--- tests/unit/session/test_parallel.py
+++ tests/unit/session/test_parallel.py
@@ -184,9 +184,5 @@ parallel_show_output = True
     result.assert_success()
     assert "stdout env" not in result.out, result.output()
     assert "stderr env" not in result.out, result.output()
-    msg = (
-        "stdout always stderr always"
-        if sys.version_info[0] == 3
-        else "stderr always stdout always"
-    )
-    assert msg in result.out, result.output()
+    assert "stdout always" in result.out, result.output()
+    assert "stderr always" in result.out, result.output()
