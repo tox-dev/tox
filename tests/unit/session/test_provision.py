f97b40f6f3cb6171f4169a2f5a72800e81332a1b
Bernat Gabor <bgabor8@bloomberg.net>
2019-07-01 12:04:07 +0100

    fix non-canonical names don't work with provisioning

--- tests/unit/session/test_provision.py
+++ tests/unit/session/test_provision.py
@@ -1,9 +1,15 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
 import shutil
 import subprocess
 import sys
 
 import py
 import pytest
+from pathlib2 import Path
+from six.moves.urllib.parse import urljoin
+from six.moves.urllib.request import pathname2url
 
 from tox.exception import BadRequirement, MissingRequirement
 
@@ -141,3 +147,92 @@ def test_provision_cli_args_not_ignored_if_provision_false(cmd, initproj):
     initproj("test-0.1", {"tox.ini": "[tox]"})
     result = cmd("-a", "--option", "b")
     result.assert_fail(is_run_test_env=False)
+
+
+@pytest.fixture(scope="session")
+def wheel(tmp_path_factory):
+    """create a wheel for a project"""
+    state = {"at": 0}
+
+    def _wheel(path):
+        state["at"] += 1
+        dest_path = tmp_path_factory.mktemp("wheel-{}-".format(state["at"]))
+        env = os.environ.copy()
+        try:
+            subprocess.check_output(
+                [
+                    sys.executable,
+                    "-m",
+                    "pip",
+                    "wheel",
+                    "-w",
+                    str(dest_path),
+                    "--no-deps",
+                    str(path),
+                ],
+                universal_newlines=True,
+                stderr=subprocess.STDOUT,
+                env=env,
+            )
+        except subprocess.CalledProcessError as exception:
+            assert not exception.returncode, exception.output
+
+        wheels = list(dest_path.glob("*.whl"))
+        assert len(wheels) == 1
+        wheel = wheels[0]
+        return wheel
+
+    return _wheel
+
+
+THIS_PROJECT_ROOT = Path(__file__).resolve().parents[3]
+
+
+@pytest.fixture(scope="session")
+def tox_wheel(wheel):
+    return wheel(THIS_PROJECT_ROOT)
+
+
+@pytest.fixture(scope="session")
+def magic_non_canonical_wheel(wheel, tmp_path_factory):
+    magic_proj = tmp_path_factory.mktemp("magic")
+    (magic_proj / "setup.py").write_text(
+        "from setuptools import setup\nsetup(name='com.magic.this-is-fun')"
+    )
+    return wheel(magic_proj)
+
+
+def test_provision_non_canonical_dep(
+    cmd, initproj, monkeypatch, tox_wheel, magic_non_canonical_wheel
+):
+    initproj(
+        "w-0.1",
+        {
+            "tox.ini": """
+            [tox]
+            envlist = py
+            requires =
+                com.magic.this-is-fun
+                tox == {}
+            [testenv:.tox]
+            passenv = *
+            """.format(
+                tox_wheel.name.split("-")[1]
+            )
+        },
+    )
+    find_links = " ".join(
+        space_path2url(d) for d in (tox_wheel.parent, magic_non_canonical_wheel.parent)
+    )
+
+    monkeypatch.setenv(str("PIP_FIND_LINKS"), str(find_links))
+
+    result = cmd("-a", "-v", "-v")
+    result.assert_success(is_run_test_env=False)
+
+
+def space_path2url(path):
+    at_path = str(path)
+    if " " not in at_path:
+        return at_path
+    return urljoin("file:", pathname2url(os.path.abspath(at_path)))
