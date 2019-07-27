41bf02f863d4950859637ab07b3948c2269e61a8
Anthony Sottile <asottile@umich.edu>
2019-07-27 14:20:47 -0700

    Improve recreate check by looking for .tox-config1

--- src/tox/venv.py
+++ src/tox/venv.py
@@ -698,7 +698,7 @@ def cleanup_for_venv(venv):
     # an error
     if venv.path.exists():
         dir_items = set(os.listdir(str(venv.path))) - {".lock", "log"}
-        dir_items = {p for p in dir_items if not p.startswith(".tox-")}
+        dir_items = {p for p in dir_items if not p.startswith(".tox-") or p == ".tox-config1"}
     else:
         dir_items = set()
 
@@ -707,6 +707,8 @@ def cleanup_for_venv(venv):
         not venv.path.exists()
         # does exist, but it's empty => OK
         or not dir_items
+        # tox has marked this as an environment it has created in the past
+        or ".tox-config1" in dir_items
         # it exists and we're on windows with Lib and Scripts => OK
         or (INFO.IS_WIN and dir_items > {"Scripts", "Lib"})
         # non-windows, with lib and bin => OK
