2c870ace6f5f88519673315b9d880e46a4433657
Pablo Galindo <pablogsal@gmail.com>
2019-09-03 16:05:38 +0100

    Correctly recognize interpreters that has suffixes

--- tests/unit/interpreters/test_py_spec.py
+++ tests/unit/interpreters/test_py_spec.py
@@ -4,3 +4,8 @@ from tox.interpreters.py_spec import PythonSpec
 def test_py_3_10():
     spec = PythonSpec.from_name("python3.10")
     assert (spec.major, spec.minor) == (3, 10)
+
+
+def test_debug_python():
+    spec = PythonSpec.from_name("python3.10-dbg")
+    assert (spec.major, spec.minor) == (None, None)
