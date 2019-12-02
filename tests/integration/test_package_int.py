1b3d3feacb5ca0c5ee1227bcdc435a5a4c31f9c4
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2019-12-02 16:16:42 +0100

    Mark poetry tests as xfail on windows

--- tests/integration/test_package_int.py
+++ tests/integration/test_package_int.py
@@ -78,6 +78,10 @@ def test_package_flit(initproj, cmd):
 
 @pytest.mark.network
 @pytest.mark.skipif(sys.version_info < (3, 0), reason="poetry is Python 3 only")
+@pytest.mark.xfail(
+    os.name == "nt",
+    reason="https://github.com/tobgu/pyrsistent/issues/88 breaks easy poetry install",
+)
 def test_package_poetry(initproj, cmd):
     initproj(
         "magic-0.1",
