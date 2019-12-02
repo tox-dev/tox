4852ba205676e54252a79520367abeaa1fa01327
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-12-02 17:27:03 +0100

    Update tests/integration/test_package_int.py
    Co-Authored-By: Anthony Sottile <asottile@umich.edu>

--- tests/integration/test_package_int.py
+++ tests/integration/test_package_int.py
@@ -80,7 +80,7 @@ def test_package_flit(initproj, cmd):
 @pytest.mark.skipif(sys.version_info < (3, 0), reason="poetry is Python 3 only")
 @pytest.mark.xfail(
     os.name == "nt",
-    reason="https://github.com/tobgu/pyrsistent/issues/88 breaks easy poetry install",
+    reason="https://github.com/tobgu/pyrsistent/issues/88 breaks poetry install",
 )
 def test_package_poetry(initproj, cmd):
     initproj(
