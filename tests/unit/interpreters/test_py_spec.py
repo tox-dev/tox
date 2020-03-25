44e93ab291625c82ecdf36152ab5a271f9f2ed8a
Charles Brunet <charles.brunet@optelgroup.com>
2020-03-25 14:43:43 -0400

    Fix parsing of architecture in python interpreter name

--- tests/unit/interpreters/test_py_spec.py
+++ tests/unit/interpreters/test_py_spec.py
@@ -9,3 +9,8 @@ def test_py_3_10():
 def test_debug_python():
     spec = PythonSpec.from_name("python3.10-dbg")
     assert (spec.major, spec.minor) == (None, None)
+
+
+def test_parse_architecture():
+    spec = PythonSpec.from_name("python3.10-32")
+    assert (spec.major, spec.minor, spec.architecture) == (3, 10, 32)
