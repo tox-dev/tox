28b5d4997bf212679adad8d6cc7efaa94c0f4bc5
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-11-04 23:51:44 -0500

    Implement full test coverage for tox_env/info.py

---
+++ tests/tox_env/test_info.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+from tox.tox_env.info import Info
+
+def test_Info_dunder_repr_method() -> None:
+    info_object = Info(Path())
+    assert repr(info_object) == 'Info(path=.tox-info.json)'
