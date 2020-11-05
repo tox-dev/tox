5905849524e66f1393a4fc184b0b82710ce28dbd
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-11-04 23:08:00 -0500

    Add full coverage for the request.py file

---
+++ tests/execute/test_request.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from tox.execute.request import ExecuteRequest
+
+def test_ExecuteRequest_constructor_raises_ValueError_if_empty_cmd_passed() -> None:
+    with pytest.raises(ValueError, match="cannot execute an empty command"):
+        ExecuteRequest(cmd=[], cwd=Path().absolute(), env=os.environ.copy(), allow_stdin=False)
