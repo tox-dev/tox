d8deef793fa3b0d7f8495ded7cf391d1e850e268
Bernát Gábor <bgabor8@bloomberg.net>
2020-11-05 06:42:00 +0000

    Simplify naming
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,21 +1,19 @@
 import pytest
 from pytest_mock import MockerFixture
 
-import tox.run
 from tox.report import HandledError
+from tox.run import run
 
 
 @pytest.mark.parametrize("exception", [HandledError, KeyboardInterrupt])
-def test_run_raises_SystemExit_with_negative_two_exit_code_when_main_throws_expected_exception(
-    exception: Exception, mocker: MockerFixture
-) -> None:
-    mocker.patch.object(tox.run, "main", side_effect=exception)
+def test_exit_code_minus_2_on_expected_exit(exception: Exception, mocker: MockerFixture) -> None:
+    mocker.patch("tox.run.main", side_effect=exception)
     with pytest.raises(SystemExit) as system_exit:
-        tox.run.run()
+        run()
     assert system_exit.value.code == -2
 
 
-def test_run_reraises_when_main_throws_unexpected_exception(mocker: MockerFixture) -> None:
-    mocker.patch.object(tox.run, "main", side_effect=ValueError)
+def test_re_raises_on_unexpected_exit(mocker: MockerFixture) -> None:
+    mocker.patch("tox.run.main", side_effect=ValueError)
     with pytest.raises(ValueError):
-        tox.run.run()
+        run()
