3e6ce09b8770531a9cd32d40b86994fb4ea01487
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-11-05 01:43:39 -0500

    tests: add coverage for tox.info
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bernát Gábor <bgabor8@bloomberg.net>

---
+++ tests/tox_env/test_info.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from tox.tox_env.info import Info
+
+
+def test_info_repr() -> None:
+    at_loc = Path().absolute()
+    info_object = Info(at_loc)
+    assert repr(info_object) == f"Info(path={ at_loc / '.tox-info.json' })"
