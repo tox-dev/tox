tox-dev/tox

commit e4d0d60070a4a88b3ca051634c7a01df333af238

Allow {posargs} in setenv (#1697)

src/tox/config/__init__.py
@@ -240,8 +240,8 @@ class PosargsOption:
 
 class InstallcmdOption:
     name = "install_command"
-    type = "argv"
-    default = "python -m pip install {opts} {packages}"
+    type = "argv_install_command"
+    default = r"python -m pip install \{opts\} \{packages\}"
     help = "install command for dependencies and package under test."
 
     def postprocess(self, testenv_config, value):
@@ -1374,6 +1374,7 @@ class ParseIni(object):
                     "dict_setenv",
                     "argv",
                     "argvlist",
+                    "argv_install_command",
                 ):
                     meth = getattr(reader, "get{}".format(atype))
                     res = meth(env_attr.name, env_attr.default, replace=replace)
@@ -1558,7 +1559,15 @@ is_section_substitution = re.compile(r"{\[[^{}\s]+\]\S+?}").match
 
 
 class SectionReader:
-    def __init__(self, section_name, cfgparser, fallbacksections=None, factors=(), prefix=None):
+    def __init__(
+        self,
+        section_name,
+        cfgparser,
+        fallbacksections=None,
+        factors=(),
+        prefix=None,
+        posargs="",
+    ):
         if prefix is None:
             self.section_name = section_name
         else:
@@ -1569,6 +1578,7 @@ class SectionReader:
         self._subs = {}
         self._subststack = []
         self._setenv = None
+        self.posargs = posargs
 
     def get_environ_value(self, name):
         if self._setenv is None:
@@ -1661,6 +1671,15 @@ class SectionReader:
     def getargv(self, name, default="", replace=True):
         return self.getargvlist(name, default, replace=replace)[0]
 
+    def getargv_install_command(self, name, default="", replace=True):
+        s = self.getstring(name, default, replace=False)
+        if "{packages}" in s:
+            s = s.replace("{packages}", r"\{packages\}")
+        if "{opts}" in s:
+            s = s.replace("{opts}", r"\{opts\}")
+
+        return _ArgvlistReader.getargvlist(self, s, replace=replace)[0]
+
     def getstring(self, name, default=None, replace=True, crossonly=False, no_fallback=False):
         x = None
         sections = [self.section_name] + ([] if no_fallback else self.fallbacksections)
@@ -1684,6 +1703,17 @@ class SectionReader:
 
         x = self._replace_if_needed(x, name, replace, crossonly)
         return x
+
+    def getposargs(self, default=None):
+        if self.posargs:
+            posargs = self.posargs
+            if sys.platform.startswith("win"):
+                posargs_string = list2cmdline([x for x in posargs if x])
+            else:
+                posargs_string = " ".join([shlex_quote(x) for x in posargs if x])
+            return posargs_string
+        else:
+            return default or ""
 
     def _replace_if_needed(self, x, name, replace, crossonly):
         if replace and x and hasattr(x, "replace"):
@@ -1771,11 +1801,8 @@ class Replacer:
         if not any(g.values()):
             return os.pathsep
 
-        # special case: opts and packages. Leave {opts} and
-        # {packages} intact, they are replaced manually in
-        # _venv.VirtualEnv.run_install_command.
-        if sub_value in ("opts", "packages"):
-            return "{{{}}}".format(sub_value)
+        if sub_value == "posargs":
+            return self.reader.getposargs(match.group("default_value"))
 
         try:
             sub_type = g["sub_type"]
@@ -1790,6 +1817,8 @@ class Replacer:
             if is_interactive():
                 return match.group("substitution_value")
             return match.group("default_value")
+        if sub_type == "posargs":
+            return self.reader.getposargs(match.group("substitution_value"))
         if sub_type is not None:
             raise tox.exception.ConfigError(
                 "No support for the {} substitution type".format(sub_type),
@@ -1883,12 +1912,6 @@ class _ArgvlistReader:
 
     @classmethod
     def processcommand(cls, reader, command, replace=True):
-        posargs = getattr(reader, "posargs", "")
-        if sys.platform.startswith("win"):
-            posargs_string = list2cmdline([x for x in posargs if x])
-        else:
-            posargs_string = " ".join([shlex_quote(x) for x in posargs if x])
-
         # Iterate through each word of the command substituting as
         # appropriate to construct the new command string. This
         # string is then broken up into exec argv components using
@@ -1896,15 +1919,10 @@ class _ArgvlistReader:
         if replace:
             newcommand = ""
             for word in CommandParser(command).words():
-                if word == "{posargs}" or word == "[]":
-                    newcommand += posargs_string
+                if word == "[]":
+                    newcommand += reader.getposargs()
                     continue
-                elif word.startswith("{posargs:") and word.endswith("}"):
-                    if posargs:
-                        newcommand += posargs_string
-                        continue
-                    else:
-                        word = word[9:-1]
+
                 new_arg = ""
                 new_word = reader._replace(word)
                 new_word = reader._replace(new_word)

tests/unit/config/test_config.py
@@ -441,6 +441,21 @@ class TestIniParserAgainstCommandsKey:
         assert envconfig.commands == [["ls", "testvalue"]]
         assert envconfig.setenv["TEST"] == "testvalue"
 
+    def test_command_env_substitution_posargs(self, newconfig):
+        """Ensure {posargs} values are substituted correctly."""
+        config = newconfig(
+            """
+           [testenv:py27]
+           setenv =
+             TEST={posargs:default}
+           commands =
+             ls {env:TEST}
+        """,
+        )
+        envconfig = config.envconfigs["py27"]
+        assert envconfig.commands == [["ls", "default"]]
+        assert envconfig.setenv["TEST"] == "default"
+
     def test_command_env_substitution_global(self, newconfig):
         """Ensure referenced {env:key:default} values are substituted correctly."""
         config = newconfig(

commit 7f13cf609dd24b18b45710f2642cf262cac958fd

config: Fix parsing posargs default with colon
e4d0d60 introduced a regression when posargs default
contained a colon, as the common substitution arg
regex splits on the colon, and uses different replace
types depending on the number of colons found.

Fixes https://github.com/tox-dev/tox/issues/1785

src/tox/config/__init__.py
@@ -1852,6 +1852,13 @@ class Replacer:
             return self.reader.getposargs(default_value)
 
         sub_type = g["sub_type"]
+        if sub_type == "posargs":
+            if default_value:
+                value = "{}:{}".format(sub_value, default_value)
+            else:
+                value = sub_value
+            return self.reader.getposargs(value)
+
         if not sub_type and not sub_value:
             raise tox.exception.ConfigError(
                 "Malformed substitution; no substitution type provided. "

tests/unit/config/test_config.py
@@ -685,7 +685,7 @@ class TestIniParserAgainstCommandsKey:
             """,
         )
         reader = SectionReader("testenv", config._cfg)
-        reader.addsubstitutions([r"argpos"])
+        reader.addsubstitutions(["argpos"])
         x = reader.getargvlist("commands")
         assert x == [["thing", "argpos", "arg2"]]
 
@@ -701,9 +701,22 @@ class TestIniParserAgainstCommandsKey:
             """,
         )
         reader = SectionReader("testenv", config._cfg)
-        reader.addsubstitutions([r"argpos"])
+        reader.addsubstitutions(["argpos"])
         x = reader.getargvlist("commands")
         assert x == [["thing", "arg1", "argpos", "endarg"]]
+
+    def test_command_posargs_with_colon(self, newconfig):
+        """Ensure posargs with default containing : succeeds"""
+        config = newconfig(
+            r"""
+            [testenv]
+            commands =
+                pytest {posargs:default with : colon after}
+            """,
+        )
+        reader = SectionReader("testenv", config._cfg)
+        x = reader.getargvlist("commands")
+        assert x[0] == ["pytest", "default", "with", ":", "colon", "after"]
 
     def test_command_missing_substitution(self, newconfig):
         config = newconfig(
@@ -747,8 +760,38 @@ class TestIniParserAgainstCommandsKey:
         """,
         )
         envconfig = config.envconfigs["py27"]
-        assert envconfig.commands == [["ls", "default"]]
         assert envconfig.setenv["TEST"] == "default"
+        assert envconfig.commands == [["ls", "default"]]
+
+    def test_command_env_substitution_posargs_with_colon(self, newconfig):
+        """Ensure {posargs} values are substituted correctly."""
+        config = newconfig(
+            """
+           [testenv:py27]
+           setenv =
+             TEST=pytest {posargs:default with:colon after}
+           commands =
+             ls {env:TEST}
+        """,
+        )
+        envconfig = config.envconfigs["py27"]
+        assert envconfig.setenv["TEST"] == "pytest default with:colon after"
+        assert envconfig.commands == [["ls", "pytest", "default", "with:colon", "after"]]
+
+    def test_command_env_substitution_posargs_with_spaced_colon(self, newconfig):
+        """Ensure {posargs} values are substituted correctly."""
+        config = newconfig(
+            """
+           [testenv:py27]
+           setenv =
+             TEST=pytest {posargs:default with : colon after}
+           commands =
+             ls {env:TEST}
+        """,
+        )
+        envconfig = config.envconfigs["py27"]
+        assert envconfig.setenv["TEST"] == "pytest default with : colon after"
+        assert envconfig.commands == [["ls", "pytest", "default", "with", ":", "colon", "after"]]
 
     def test_command_env_substitution_global(self, newconfig):
         """Ensure referenced {env:key:default} values are substituted correctly."""