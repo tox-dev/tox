8bbbd324981cd27a0ec538227884b499d9ca7f33
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-02 12:31:53 +0000

    Ensure example pyproject.toml can be correctly written on Python 2

--- tests/unit/config/test_config.py
+++ tests/unit/config/test_config.py
@@ -3557,7 +3557,7 @@ def test_config_via_pyproject_legacy(initproj):
     initproj(
         "config_via_pyproject_legacy-0.5",
         filedefs={
-            "pyproject.toml": '''
+            "pyproject.toml": u'''
                 [project]
                 description = "Factory ⸻ A code generator 🏭"
                 authors = [{name = "Łukasz Langa"}]
