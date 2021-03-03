d91d23177b1ee2e2bceca7c8a37f482d1d8e9ef4
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-03-03 15:41:11 +0300

    Add more cases to test_replace_pos_args_escaped

--- tests/config/loader/ini/replace/test_replace_posargs.py
+++ tests/config/loader/ini/replace/test_replace_posargs.py
@@ -59,9 +59,12 @@ def test_replace_pos_args_default(replace_one: ReplaceOne, value: str, result: s
         "\\{posargs\\}",
         "{\\{posargs}",
         "{\\{posargs}{}",
+        pytest.param("\\[]", marks=pytest.mark.xfail(raises=AssertionError)),  # noqa: SC200
+        pytest.param("[\\]", marks=pytest.mark.xfail(raises=AssertionError)),  # noqa: SC200
+        pytest.param("\\[\\]", marks=pytest.mark.xfail(raises=AssertionError)),  # noqa: SC200
     ],
 )
 def test_replace_pos_args_escaped(replace_one: ReplaceOne, value: str) -> None:
     result = replace_one(value, None)
-    outcome = value.replace("\\{", "{").replace("\\}", "}")
+    outcome = value.replace("\\", "")
     assert result == outcome
