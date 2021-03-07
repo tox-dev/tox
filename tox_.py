4df586b3e8dade102c5800fc6c303eab4c5168bc
Bernát Gábor <gaborjbernat@gmail.com>
2021-03-07 22:53:36 +0000

    Remove test tox_.py
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tox_.py
+++
@@ -1,7 +0,0 @@
-from tox.config.cli.parser import ToxParser
-from tox.plugin.impl import impl
-
-
-@impl
-def tox_add_option(parser: ToxParser) -> None:
-    parser.add_argument("--magic", action="store_true", help="provides some magic")
