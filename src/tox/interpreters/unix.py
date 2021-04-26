30642f67a295b4ff2098423286d930a434ca08bd
Jack Desert <jack.desert>
2021-04-26 14:27:29 -0400

    Fail if Specified Interpreter Not Found (Do NOT use basepython as a fallback)
    This is intended to remedy these two github issues:
    
    https://github.com/tox-dev/tox/issues/882
    https://github.com/tox-dev/tox/issues/1484
    
    by :user: jackdesert

--- src/tox/interpreters/unix.py
+++ src/tox/interpreters/unix.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
+import sys
 
+from tox import reporter
 import tox
 
 from .common import base_discover
@@ -8,12 +10,15 @@ from .via_path import check_with_path
 
 @tox.hookimpl
 def tox_get_python_executable(envconfig):
+    """
+    Return path to specified interpreter.
+    If not available, exit(1).
+    """
     spec, path = base_discover(envconfig)
-    if path is not None:
+
+    if path is None:
+        reporter.error('envname {} not found'.format(envconfig.envname))
+        sys.exit(1)
+    else:
         return path
-    # 3. check if the literal base python
-    candidates = [envconfig.basepython]
-    # 4. check if the un-versioned name is good
-    if spec.name is not None and spec.name != envconfig.basepython:
-        candidates.append(spec.name)
-    return check_with_path(candidates, spec)
+~
