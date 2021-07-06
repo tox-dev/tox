3dc9b3346e5c2454be538d93b9f3f6e6a132ec7c
Rebecca Turner <rturner@starry.com>
2021-07-06 14:14:51 -0400

    Use sysconfig instead of distutils
    - `distutils` is deprecated in Python 3.10 and slated for removal in
      Python 3.12
    - `sysconfig` provides the same functionality

--- src/tox/helper/get_site_package_dir.py
+++ src/tox/helper/get_site_package_dir.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 
-import distutils.sysconfig
+import sysconfig
 import json
 import sys
 
-data = json.dumps({"dir": distutils.sysconfig.get_python_lib(prefix=sys.argv[1])})
+data = json.dumps({"dir": sysconfig.get_path("purelib", vars={"base": sys.argv[1]})})
 print(data)
