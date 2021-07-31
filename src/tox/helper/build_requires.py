27f4c3496c21ecd2cdb6c60592f7f470755a90ac
oczkoisse <oczkoisse@pm.me>
2021-07-30 21:38:02 -0700

    Fix isolated build with backends missing optional hooks

--- src/tox/helper/build_requires.py
+++ src/tox/helper/build_requires.py
@@ -12,6 +12,13 @@ backend = __import__(backend_spec, fromlist=["_trash"])
 if backend_obj:
     backend = getattr(backend, backend_obj)
 
-for_build_requires = backend.get_requires_for_build_sdist(None)
+try:
+    for_build_requires = backend.get_requires_for_build_sdist(None)
+except AttributeError:
+    # PEP 517 states that get_requires_for_build_sdist is optional for a build
+    # backend object. When the backend object omits it, the default
+    # implementation must be equivalent to return []
+    for_build_requires = []
+
 output = json.dumps(for_build_requires)
 print(output)
