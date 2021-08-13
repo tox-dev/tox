b0b7508a177448e4dc2d39881559ca974f5a03ee
Bernát Gábor <gaborjbernat@gmail.com>
2021-08-13 11:05:25 +0100

    Add support for args_are_paths
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- src/tox/session/cmd/exec_.py
+++ src/tox/session/cmd/exec_.py
@@ -1,6 +1,9 @@
 """
 Execute a command in a tox environment.
 """
+from pathlib import Path
+from typing import Optional
+
 from tox.config.cli.parser import ToxParser
 from tox.config.loader.memory import MemoryLoader
 from tox.config.types import Command
@@ -21,15 +24,18 @@ def tox_add_option(parser: ToxParser) -> None:
 
 
 def exec_(state: State) -> int:
-    if not state.conf.pos_args:
-        raise HandledError("You must specify a command as positional arguments, use -- <command>")
     env_list = list(state.env_list(everything=False))
     if len(env_list) != 1:
         raise HandledError(f"exactly one target environment allowed in exec mode but found {', '.join(env_list)}")
     loader = MemoryLoader(  # these configuration values are loaded from in-memory always (no file conf)
         commands_pre=[],
-        commands=[Command(list(state.conf.pos_args))],
+        commands=[],
         commands_post=[],
     )
-    state.conf.get_env(env_list[0], loaders=[loader])
+    conf = state.conf.get_env(env_list[0], loaders=[loader])
+    to_path: Optional[Path] = conf["change_dir"] if conf["args_are_paths"] else None
+    pos_args = state.conf.pos_args(to_path)
+    if not pos_args:
+        raise HandledError("You must specify a command as positional arguments, use -- <command>")
+    loader.raw["commands"] = [Command(list(pos_args))]
     return run_sequential(state)
--- tests/config/test_main.py
+++ tests/config/test_main.py
@@ -1,8 +1,11 @@
+import os
+
 from tests.conftest import ToxIniCreator
 from tox.config.loader.api import Override
 from tox.config.loader.memory import MemoryLoader
 from tox.config.main import Config
 from tox.config.sets import ConfigSet
+from tox.pytest import ToxProjectCreator
 
 
 def test_empty_config_repr(empty_config: Config) -> None:
@@ -53,3 +56,28 @@ def test_config_new_source(tox_ini_conf: ToxIniCreator) -> None:
     conf = main_conf.get_env("py", loaders=[MemoryLoader(c="something_else")])
     conf.add_config("c", of_type=str, default="d", desc="desc")
     assert conf["c"] == "something_else"
+
+
+def test_args_are_paths_when_disabled(tox_project: ToxProjectCreator) -> None:
+    ini = "[testenv]\npackage=skip\ncommands={posargs}\nargs_are_paths=False"
+    project = tox_project({"tox.ini": ini, "w": {"a.txt": "a"}})
+    args = "magic.py", str(project.path), f"..{os.sep}tox.ini", "..", f"..{os.sep}.."
+    result = project.run("c", "-e", "py", "-k", "commands", "--", *args, from_cwd=project.path / "w")
+    result.assert_success()
+    assert result.out == f"[testenv:py]\ncommands = magic.py {project.path} ..{os.sep}tox.ini .. ..{os.sep}..\n"
+
+
+def test_args_are_paths_when_from_child_dir(tox_project: ToxProjectCreator) -> None:
+    project = tox_project({"tox.ini": "[testenv]\npackage=skip\ncommands={posargs}", "w": {"a.txt": "a"}})
+    args = "magic.py", str(project.path), f"..{os.sep}tox.ini", "..", f"..{os.sep}.."
+    result = project.run("c", "-e", "py", "-k", "commands", "--", *args, from_cwd=project.path / "w")
+    result.assert_success()
+    assert result.out == f"[testenv:py]\ncommands = magic.py {project.path} tox.ini . ..\n"
+
+
+def test_args_are_paths_when_with_change_dir(tox_project: ToxProjectCreator) -> None:
+    project = tox_project({"tox.ini": "[testenv]\npackage=skip\ncommands={posargs}\nchange_dir=w", "w": {"a.txt": "a"}})
+    args = "magic.py", str(project.path), "tox.ini", f"w{os.sep}a.txt", "w", "."
+    result = project.run("c", "-e", "py", "-k", "commands", "--", *args)
+    result.assert_success()
+    assert result.out == f"[testenv:py]\ncommands = magic.py {project.path} ..{os.sep}tox.ini a.txt . ..\n"
