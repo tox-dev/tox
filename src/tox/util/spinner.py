db680f8cdb96a61af90727026ab0e5e8fb68d90a
Brett Smith <brettcsmith@brettcsmith.org>
2021-08-19 21:23:38 -0400

    Add SpinnerMessage helper class
    This class provides a single interface for spinner strings where we
    ideally want to report Unicode but want to fall back to ASCII when the
    terminal doesn't support it.

--- src/tox/util/spinner.py
+++ src/tox/util/spinner.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 import threading
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 from datetime import datetime
 
 import py
@@ -19,34 +19,29 @@ if os.name == "nt":
         _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
 
 
-def _file_support_encoding(chars, file):
-    encoding = getattr(file, "encoding", None)
-    if encoding is not None:
-        for char in chars:
-            try:
-                char.encode(encoding)
-            except UnicodeEncodeError:
-                break
+_BaseMessage = namedtuple("_BaseMessage", ["unicode_msg", "ascii_msg"])
+
+
+class SpinnerMessage(_BaseMessage):
+    def for_file(self, file):
+        try:
+            self.unicode_msg.encode(file.encoding)
+        except (AttributeError, TypeError, UnicodeEncodeError):
+            return self.ascii_msg
         else:
-            return True
-    return False
+            return self.unicode_msg
 
 
 class Spinner(object):
     CLEAR_LINE = "\033[K"
     max_width = 120
-    UNICODE_FRAMES = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
-    ASCII_FRAMES = ["|", "-", "+", "x", "*"]
+    FRAMES = SpinnerMessage("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏", "|-+x*")
 
     def __init__(self, enabled=True, refresh_rate=0.1):
         self.refresh_rate = refresh_rate
         self.enabled = enabled
         self._file = sys.stdout
-        self.frames = (
-            self.UNICODE_FRAMES
-            if _file_support_encoding(self.UNICODE_FRAMES, sys.stdout)
-            else self.ASCII_FRAMES
-        )
+        self.frames = self.FRAMES.for_file(self._file)
         self.stream = py.io.TerminalWriter(file=self._file)
         self._envs = OrderedDict()
         self._frame_index = 0
