d44f9cca5b77985bc9b42146ef43aa991bdfd6fb
Bernát Gábor <gaborjbernat@gmail.com>
2021-12-28 12:12:00 +0000

    Move CI to Github Actions
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/unit/package/builder/test_package_builder_legacy.py
+++ tests/unit/package/builder/test_package_builder_legacy.py
@@ -1,6 +1,3 @@
-import os
-
-
 def test_verbose_legacy_build(initproj, mock_venv, cmd):
     initproj(
         "example123-0.5",
@@ -14,4 +11,4 @@ def test_verbose_legacy_build(initproj, mock_venv, cmd):
     result = cmd("--sdistonly", "-vvv", "-e", "py")
     assert "running sdist" in result.out, result.out
     assert "running egg_info" in result.out, result.out
-    assert "Writing example123-0.5{}setup.cfg".format(os.sep) in result.out, result.out
+    assert "removing 'example123-0.5'" in result.out, result.out
