93e5782aa22861816f1b17820b702cba2eba0a7d
mxd4 <maxime_droy@ultimatesoftware.com>
2022-05-06 18:35:41 +0200

    Display registered plugins with --version

--- src/tox/session/cmd/version_flag.py
+++ src/tox/session/cmd/version_flag.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 from tox.config.cli.parser import ToxParser
 from tox.plugin import impl
+from tox.plugin.manager import MANAGER
 
 
 @impl
@@ -17,5 +18,17 @@ def tox_add_option(parser: ToxParser) -> None:
     parser.add_argument(
         "--version",
         action="version",
-        version=f"{version} from {Path(tox.__file__).absolute()}",
+        version=f"{version} from {Path(tox.__file__).absolute()}.\n{get_registered_plugins()}",
     )
+
+
+def get_registered_plugins():
+    out = ["registered plugins:"]
+    plugin_dist_info = MANAGER.manager.list_plugin_distinfo()
+    if plugin_dist_info:
+        for mod, egg_info in plugin_dist_info:
+            source = getattr(mod, "__file__", repr(mod))
+            out.append("    {}-{} at {}".format(egg_info.project_name, egg_info.version, source))
+    else:
+        out.append("None.")
+    return "\n".join(out)
