1e70d1f4f0987f63dc16844aa6e79e428da0c84c
mxd4 <maxime_droy@ultimatesoftware.com>
2022-05-08 12:49:42 +0200

    Import __version__ locally in tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from _pytest.monkeypatch import MonkeyPatch
 
-from tox import __version__
 from tox.plugin.manager import MANAGER
 from tox.pytest import ToxProjectCreator
 
@@ -14,6 +13,8 @@ def test_version() -> None:
 
 
 def test_version_without_plugin(tox_project: ToxProjectCreator) -> None:
+    from tox import __version__
+
     outcome = tox_project({"tox.ini": ""}).run("--version")
     outcome.assert_success()
     assert __version__ in outcome.out
@@ -21,6 +22,8 @@ def test_version_without_plugin(tox_project: ToxProjectCreator) -> None:
 
 
 def test_version_with_plugin(tox_project: ToxProjectCreator, monkeypatch: MonkeyPatch) -> None:
+    from tox import __version__
+
     def dummy_plugin():
         class MockModule:
             __file__ = "dummy-path"
