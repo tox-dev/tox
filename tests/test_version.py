2a99959bba33d43370f5cffeecda5b70d316d1fd
Bernát Gábor <gaborjbernat@gmail.com>
2022-05-13 09:53:59 +0100

    PR Feedback
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,42 +1,44 @@
 from __future__ import annotations
 
-from unittest import mock
+from types import SimpleNamespace
 
+from pytest_mock import MockFixture
+
+from tox import __version__
 from tox.plugin.manager import MANAGER
 from tox.pytest import ToxProjectCreator
 
 
 def test_version() -> None:
-    from tox import __version__
-
     assert __version__
 
 
 def test_version_without_plugin(tox_project: ToxProjectCreator) -> None:
-    from tox import __version__
-
     outcome = tox_project({"tox.ini": ""}).run("--version")
     outcome.assert_success()
     assert __version__ in outcome.out
     assert "plugin" not in outcome.out
 
 
-def test_version_with_plugin(tox_project: ToxProjectCreator) -> None:
-    from tox import __version__
+def test_version_with_plugin(tox_project: ToxProjectCreator, mocker: MockFixture) -> None:
+    dist = [
+        (
+            mocker.create_autospec("types.ModuleType", __file__=f"{i}-path"),
+            SimpleNamespace(project_name=i, version=v),
+        )
+        for i, v in (("B", "1.0"), ("A", "2.0"))
+    ]
+    mocker.patch.object(MANAGER.manager, "list_plugin_distinfo", return_value=dist)
 
-    mock_module = mock.Mock(__file__="dummy-path")
-
-    mock_egg_info = mock.Mock(
-        project_name="dummy-project",
-        version="1.0",
-    )
-
-    with mock.patch.object(MANAGER.manager, "list_plugin_distinfo", return_value=[(mock_module, mock_egg_info)]):
-        outcome = tox_project({"tox.ini": ""}).run("--version")
+    outcome = tox_project({"tox.ini": ""}).run("--version")
 
     outcome.assert_success()
-    assert __version__ in outcome.out
-    assert "registered plugins:" in outcome.out
-    assert "dummy-path" in outcome.out
-    assert "dummy-project" in outcome.out
-    assert "1.0" in outcome.out
+    assert not outcome.err
+    lines = outcome.out.splitlines()
+    assert lines[0].startswith(__version__)
+
+    assert lines[1:] == [
+        "registered plugins:",
+        "    B-1.0 at B-path",
+        "    A-2.0 at A-path",
+    ]
