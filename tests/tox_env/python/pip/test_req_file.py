c0f00e7fa0799373db0aad3b13e67ba3e8024a1b
Masen Furer <m_github@0x26.net>
2022-11-25 03:01:36 -0800

    [test case] tox4: fails to process requirement files with --hash
    Add a test case for issue #2373
    
    * Specifying `--hash` in the deps list doesn't work (pip would
      reject this anyway).
    * Specifying `--hash` in a requirements.txt file named in the deps list
      should work, and recursive parsing should correctly extract the hash.

--- tests/tox_env/python/pip/test_req_file.py
+++ tests/tox_env/python/pip/test_req_file.py
@@ -14,3 +14,31 @@ def test_legacy_requirement_file(tmp_path: Path, legacy_flag: str) -> None:
     assert python_deps.as_root_args == [legacy_flag, "a.txt"]
     assert vars(python_deps.options) == {}
     assert [str(i) for i in python_deps.requirements] == ["b" if legacy_flag == "-r" else "-c b"]
+
+
+def test_deps_with_hash(tmp_path: Path) -> None:
+    """deps with --hash should raise an exception."""
+    python_deps = PythonDeps(
+        raw="foo==1 --hash sha256:97a702083b0d906517b79672d8501eee470d60ae55df0fa9d4cfba56c7f65a82",
+        root=tmp_path,
+    )
+    with pytest.raises(ValueError, match="Cannot use --hash in deps list"):
+        _ = python_deps.requirements
+
+
+def test_deps_with_requirements_with_hash(tmp_path: Path) -> None:
+    """deps can point to a requirements file that has --hash."""
+    exp_hash = "sha256:97a702083b0d906517b79672d8501eee470d60ae55df0fa9d4cfba56c7f65a82"
+    requirements = tmp_path / "requirements.txt"
+    requirements.write_text(
+        f"foo==1 --hash {exp_hash}",
+    )
+    python_deps = PythonDeps(
+        raw="-r requirements.txt",
+        root=tmp_path,
+    )
+    assert len(python_deps.requirements) == 1
+    parsed_req = python_deps.requirements[0]
+    assert str(parsed_req.requirement) == "foo==1"
+    assert parsed_req.options == {"hash": [exp_hash]}
+    assert parsed_req.from_file == str(requirements)
