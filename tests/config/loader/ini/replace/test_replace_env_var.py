400f4c4dd39ec25754f338d7c1ed5d6a01723077
Masen Furer <m_github@0x26.net>
2022-12-16 23:12:58 -0800

    Double backslash should escape to backslash
    The resulting escaped value should not then be
    subsequently used as an escape in future replacement
    rounds.
    
    Additional friends, like triple backslash, which
    should escape the following, and quad backslash,
    which should escape to two single backslashes
    and not escape the following characters.
    
    The Quad backslash test case also has escaped
    double backslash at the beginning and end of the
    string for more edge coverage.

--- tests/config/loader/ini/replace/test_replace_env_var.py
+++ tests/config/loader/ini/replace/test_replace_env_var.py
@@ -11,6 +11,27 @@ def test_replace_env_set(replace_one: ReplaceOne, monkeypatch: MonkeyPatch) -> N
     assert result == "something good"
 
 
+def test_replace_env_set_double_bs(replace_one: ReplaceOne, monkeypatch: MonkeyPatch) -> None:
+    """Double backslash should escape to single backslash and not affect surrounding replacements."""
+    monkeypatch.setenv("MAGIC", "something good")
+    result = replace_one(r"{env:MAGIC}\\{env:MAGIC}")
+    assert result == r"something good\something good"
+
+
+def test_replace_env_set_triple_bs(replace_one: ReplaceOne, monkeypatch: MonkeyPatch) -> None:
+    """Triple backslash should escape to single backslash also escape subsequent replacement."""
+    monkeypatch.setenv("MAGIC", "something good")
+    result = replace_one(r"{env:MAGIC}\\\{env:MAGIC}")
+    assert result == r"something good\{env:MAGIC}"
+
+
+def test_replace_env_set_quad_bs(replace_one: ReplaceOne, monkeypatch: MonkeyPatch) -> None:
+    """Quad backslash should escape to two backslashes and not affect surrounding replacements."""
+    monkeypatch.setenv("MAGIC", "something good")
+    result = replace_one(r"\\{env:MAGIC}\\\\{env:MAGIC}\\")
+    assert result == r"\something good\\something good" + "\\"
+
+
 def test_replace_env_missing(replace_one: ReplaceOne, monkeypatch: MonkeyPatch) -> None:
     """If we have a factor that is not specified within the core env-list then that's also an environment"""
     monkeypatch.delenv("MAGIC", raising=False)
