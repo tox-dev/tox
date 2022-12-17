24645ef9805a65f32e15ee6d22f9109abe7d85f5
Masen Furer <m_github@0x26.net>
2022-12-16 23:14:13 -0800

    test_replace_os_sep_before_curly
    explicit test case for issue #2732 which affects
    {/} os.sep replacement on windows platform.

--- tests/config/loader/ini/replace/test_replace_os_sep.py
+++ tests/config/loader/ini/replace/test_replace_os_sep.py
@@ -2,9 +2,20 @@ from __future__ import annotations
 
 import os
 
+import pytest
+
 from tests.config.loader.ini.replace.conftest import ReplaceOne
 
 
 def test_replace_os_sep(replace_one: ReplaceOne) -> None:
     result = replace_one("{/}")
     assert result == os.sep
+
+
+@pytest.mark.parametrize("sep", ["/", "\\"])
+def test_replace_os_sep_before_curly(monkeypatch, replace_one: ReplaceOne, sep: str) -> None:
+    """Explicit test case for issue #2732 (windows only)."""
+    monkeypatch.setattr(os, "sep", sep)
+    monkeypatch.delenv("_", raising=False)
+    result = replace_one("{/}{env:_:foo}")
+    assert result == os.sep + "foo"
