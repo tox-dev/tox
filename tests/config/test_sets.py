f23e42fed2d9f8cfe2bd98a41acb7b8f2253e7c3
q0w <43147888+q0w@users.noreply.github.com>
2022-12-26 06:24:28 +0300

    Override toxworkdir with --workdir.

--- tests/config/test_sets.py
+++ tests/config/test_sets.py
@@ -173,9 +173,9 @@ def test_set_env_raises_on_non_str(mocker: MockerFixture) -> None:
         assert env_set["set_env"]
 
 
-@pytest.mark.parametrize("work_dir", ["a", ""])
-def test_config_work_dir(tox_project: ToxProjectCreator, work_dir: str) -> None:
+@pytest.mark.parametrize("work_dir", [Path("a"), None])
+def test_config_work_dir(tox_project: ToxProjectCreator, work_dir: Path | None) -> None:
     project = tox_project({"tox.ini": "[tox]\ntoxworkdir=b"})
-    result = project.run("c", *(["--workdir", work_dir] if work_dir else []))
+    result = project.run("c", *(["--workdir", str(work_dir)] if work_dir else []))
     expected = Path(project.path, work_dir) if work_dir else Path("b")
     assert expected == result.state.conf.core["work_dir"]
