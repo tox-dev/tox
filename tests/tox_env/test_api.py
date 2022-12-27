6901b09db5de44991280d64c8bfcfb9bb3814e13
q0w <43147888+q0w@users.noreply.github.com>
2022-12-27 05:39:10 +0300

    Fix tests

---
+++ tests/tox_env/test_api.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from tox.pytest import ToxProjectCreator
+
+
+def test_ensure_temp_dir_exists(tox_project: ToxProjectCreator) -> None:
+    ini = "[testenv]\ncommands=python -c 'print(1)'"
+    project = tox_project({"tox.ini": ini})
+    result = project.run()
+    result.assert_success()
+    assert result.state.conf.core["temp_dir"].exists()
+
+
+def test_dont_cleanup_temp_dir(tox_project: ToxProjectCreator, tmp_path: Path) -> None:
+    ini = f"[tox]\ntemp_dir={tmp_path}\n[testenv]\ncommands=python -c 'print(1)'"
+    (tmp_path / "foo").mkdir()
+    project = tox_project({"tox.ini": ini})
+    result = project.run()
+    result.assert_success()
+    assert result.state.conf.core["temp_dir"].exists()
+    assert (result.state.conf.core["temp_dir"] / "foo").exists()
