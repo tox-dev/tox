36e738022d4a6d9b2c97e2d6369511d44d42f253
Masen Furer <m_github@0x26.net>
2022-12-30 11:24:02 -0800

    test_replace: fixup expectations and additional high level cases
    test_replace: {toxinidir,}
    	shouldn't be replaced with the trailing comma
    
    test_replace: add test case for escaped colon arg sep
    
    test_replace: additional cases for test_match
    
    	Single capital letter variable
    	Single backslash
    	Backslash escape non-special character
    	Windows path shouldn't be mangled

--- tests/config/loader/ini/replace/test_replace.py
+++ tests/config/loader/ini/replace/test_replace.py
@@ -2,26 +2,79 @@ from __future__ import annotations
 
 import pytest
 
+from tests.config.loader.ini.replace.conftest import ReplaceOne
 from tox.config.loader.ini.replace import MatchExpression, find_replace_expr
+from tox.report import HandledError
 
 
 @pytest.mark.parametrize(
     ("value", "exp_output"),
     [
-        ("[]", [[MatchExpression([["posargs"]])]]),
-        ("123[]", [["123", MatchExpression([["posargs"]])]]),
-        ("[]123", [[MatchExpression([["posargs"]]), "123"]]),
-        (r"\[\] []", [["[] ", MatchExpression([["posargs"]])]]),
-        (r"[\] []", [["[] ", MatchExpression([["posargs"]])]]),
-        (r"\[] []", [["[] ", MatchExpression([["posargs"]])]]),
-        ("{foo}", [[MatchExpression([["foo"]])]]),
-        (r"\{foo} {bar}", [["{foo} ", MatchExpression([["bar"]])]]),
-        ("{foo} {bar}", [[MatchExpression([["foo"]]), " ", MatchExpression([["bar"]])]]),
-        (r"{foo\} {bar}", [["{foo} ", MatchExpression([["bar"]])]]),
-        (r"{foo:{bar}}", [[MatchExpression([["foo"], [MatchExpression([["bar"]])]])]]),
-        (r"{\{}", [[MatchExpression([["{"]])]]),
-        (r"{\}}", [[MatchExpression([["}"]])]]),
+        ("[]", [MatchExpression([["posargs"]])]),
+        ("123[]", ["123", MatchExpression([["posargs"]])]),
+        ("[]123", [MatchExpression([["posargs"]]), "123"]),
+        (r"\[\] []", ["[] ", MatchExpression([["posargs"]])]),
+        (r"[\] []", ["[] ", MatchExpression([["posargs"]])]),
+        (r"\[] []", ["[] ", MatchExpression([["posargs"]])]),
+        ("{foo}", [MatchExpression([["foo"]])]),
+        (r"\{foo} {bar}", ["{foo} ", MatchExpression([["bar"]])]),
+        ("{foo} {bar}", [MatchExpression([["foo"]]), " ", MatchExpression([["bar"]])]),
+        (r"{foo\} {bar}", ["{foo} ", MatchExpression([["bar"]])]),
+        (r"{foo:{bar}}", [MatchExpression([["foo"], [MatchExpression([["bar"]])]])]),
+        (r"{foo\::{bar}}", [MatchExpression([["foo:"], [MatchExpression([["bar"]])]])]),
+        (r"{foo:B:c:D:e}", [MatchExpression([["foo"], ["B"], ["c"], ["D"], ["e"]])]),
+        (r"{\{}", [MatchExpression([["{"]])]),
+        (r"{\}}", [MatchExpression([["}"]])]),
+        (
+            r"p{foo:b{a{r}:t}:{ba}z}s",
+            [
+                "p",
+                MatchExpression(
+                    [
+                        ["foo"],
+                        [
+                            "b",
+                            MatchExpression(
+                                [
+                                    ["a", MatchExpression([["r"]])],
+                                    ["t"],
+                                ],
+                            ),
+                        ],
+                        [
+                            MatchExpression(
+                                [["ba"]],
+                            ),
+                            "z",
+                        ],
+                    ],
+                ),
+                "s",
+            ],
+        ),
+        ("\\", ["\\"]),
+        (r"\d", ["\\d"]),
+        (r"C:\WINDOWS\foo\bar", [r"C:\WINDOWS\foo\bar"]),
     ],
 )
 def test_match(value: str, exp_output: list[str | MatchExpression]) -> None:
-    assert find_replace_expr(value) == (exp_output, len(value))
+    assert find_replace_expr(value) == exp_output
+
+
+@pytest.mark.parametrize(
+    ("value", "exp_exception"),
+    [
+        ("py-{foo,bar}", None),
+        ("py37-{base,i18n},b", None),
+        ("py37-{i18n,base},b", None),
+        ("{toxinidir,}", None),
+        ("{env}", r"MatchError\('No variable name was supplied in {env} substitution'\)"),
+    ],
+)
+def test_dont_replace(replace_one: ReplaceOne, value: str, exp_exception: str | None) -> None:
+    """Test that invalid expressions are not replaced."""
+    if exp_exception:
+        with pytest.raises(HandledError, match=exp_exception):
+            replace_one(value)
+    else:
+        assert replace_one(value) == value
