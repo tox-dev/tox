8d8eba619b9e31c94fb8c09d2ab61223e935acd5
Masen Furer <m_github@0x26.net>
2023-04-05 06:23:00 -0700

    Avoid UnicodeDecodeError from command output (#2970)
    * test_sync_write_decode_surrogate: utf-8 decode
    
    When SyncWrite decodes bytes as utf-8, it should replace unknown
    sequences with the unicode surrogate codepoint instead of crashing the
    program.
    
    Test case for #2969
    
    * SyncWrite: decode with errors='surrogateescape'
    
    Avoid bubbling UnicodeDecodeError up from stream handling internals.
    
    Tox has no way of knowing that the bytestream emitted by a command will
    be valid utf-8, even if utf-8 is ostensibly the "correct" encoding for
    the stream. It's always possible for an arbitrary command to return
    non-utf-8 bytes, and this situation should not break tox.
    
    Fix #2969

--- tests/execute/test_stream.py
+++ tests/execute/test_stream.py
@@ -8,3 +8,9 @@ from tox.execute.stream import SyncWrite
 def test_sync_write_repr() -> None:
     sync_write = SyncWrite(name="a", target=None, color=Fore.RED)
     assert repr(sync_write) == f"SyncWrite(name='a', target=None, color={Fore.RED!r})"
+
+
+def test_sync_write_decode_surrogate() -> None:
+    sync_write = SyncWrite(name="a", target=None)
+    sync_write.handler(b"\xed\n")
+    assert sync_write.text == "\udced\n"
