c2a014300101105aa4a8a680082b8a09867717d3
Alex Lowe <lengau@gmail.com>
2023-06-02 22:04:46 -0400

    add params to devenv

--- src/tox/session/cmd/run/common.py
+++ src/tox/session/cmd/run/common.py
@@ -70,6 +70,19 @@ def env_run_create_flags(parser: ArgumentParser, mode: str) -> None:
             default=None,
             help="write a JSON file with detailed information about all commands and results involved",
         )
+        list_deps = parser.add_mutually_exclusive_group()
+        list_deps.add_argument(
+            "--list-dependencies",
+            action="store_true",
+            default=is_ci(),
+            help="list the dependencies installed during environment setup",
+        )
+        list_deps.add_argument(
+            "--no-list-dependencies",
+            action="store_false",
+            dest="list_dependencies",
+            help="never list the dependencies installed during environment setup",
+        )
     if mode not in ("devenv", "depends"):
         parser.add_argument(
             "-s",
@@ -163,19 +176,6 @@ def env_run_create_flags(parser: ArgumentParser, mode: str) -> None:
             dest="skip_pkg_install",
             help="skip package installation for this run",
             action="store_true",
-        )
-        list_deps = parser.add_mutually_exclusive_group()
-        list_deps.add_argument(
-            "--list-dependencies",
-            action="store_true",
-            default=is_ci(),
-            help="list the dependencies installed during environment setup",
-        )
-        list_deps.add_argument(
-            "--no-list-dependencies",
-            action="store_false",
-            dest="list_dependencies",
-            help="never list the dependencies installed during environment setup",
         )
 
 
