fc800803a1725fb4d5d6a950a3c6eb9e0555ad88
Antoine Musso <hashar@free.fr>
2024-01-12 01:16:56 +0100

    Always pass FORCE_COLOR & NO_COLOR to the environment (#3172)
    Co-authored-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/session/cmd/test_show_config.py
+++ tests/session/cmd/test_show_config.py
@@ -123,8 +123,11 @@ def test_pass_env_config_default(tox_project: ToxProjectCreator, stdout_is_atty:
         + (["APPDATA"] if is_win else [])
         + ["CC", "CCSHARED", "CFLAGS"]
         + (["COMSPEC"] if is_win else [])
-        + ["CPPFLAGS", "CURL_CA_BUNDLE", "CXX", "HOME", "LANG", "LANGUAGE", "LDFLAGS", "LD_LIBRARY_PATH"]
-        + (["MSYSTEM", "NUMBER_OF_PROCESSORS", "PATHEXT"] if is_win else [])
+        + ["CPPFLAGS", "CURL_CA_BUNDLE", "CXX", "FORCE_COLOR", "HOME", "LANG"]
+        + ["LANGUAGE", "LDFLAGS", "LD_LIBRARY_PATH"]
+        + (["MSYSTEM"] if is_win else [])
+        + ["NO_COLOR"]
+        + (["NUMBER_OF_PROCESSORS", "PATHEXT"] if is_win else [])
         + ["PIP_*", "PKG_CONFIG", "PKG_CONFIG_PATH", "PKG_CONFIG_SYSROOT_DIR"]
         + (["PROCESSOR_ARCHITECTURE"] if is_win else [])
         + (["PROGRAMDATA"] if is_win else [])
