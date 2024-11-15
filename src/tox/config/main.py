b2c3c504e087cb3f8d6431d951b891cccf98cd74
Henry Schreiner <henryschreineriii@gmail.com>
2024-11-15 02:09:07 -0500

    feat: adding a schema command
    Now running this passes:
    
        uvx check-jsonschema --schemafile src/tox/tox.schema.json tox.toml
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/tox/config/main.py
+++ src/tox/config/main.py
@@ -123,6 +123,9 @@ class Config:
         self._core_set = core
         return core
 
+    def all_section_configs(self) -> Iterator[tuple[tuple[str, str, str], ConfigSet]]:
+        yield from self._key_to_conf_set.items()
+
     def get_section_config(
         self,
         section: Section,
