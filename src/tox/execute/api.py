ee660b96bdcf527f4706c9e406d25e1dcb54048b
Sorin Sbarnea <ssbarnea@redhat.com>
2025-01-21 18:38:43 +0000

    Adds ability to configure stderr output color (#3426)

--- src/tox/execute/api.py
+++ src/tox/execute/api.py
@@ -122,18 +122,19 @@ class Execute(ABC):
         env: ToxEnv,
     ) -> Iterator[ExecuteStatus]:
         start = time.monotonic()
+        stderr_color = None
+        if self._colored:
+            try:
+                cfg_color = env.conf._conf.options.stderr_color  # noqa: SLF001
+                stderr_color = getattr(Fore, cfg_color)
+            except (AttributeError, KeyError, TypeError):  # many tests have a mocked 'env'
+                stderr_color = Fore.RED
         try:
             # collector is what forwards the content from the file streams to the standard streams
             out, err = out_err[0].buffer, out_err[1].buffer
-            out_sync = SyncWrite(
-                out.name,
-                out if show else None,  # type: ignore[arg-type]
-            )
-            err_sync = SyncWrite(
-                err.name,
-                err if show else None,  # type: ignore[arg-type]
-                Fore.RED if self._colored else None,
-            )
+            out_sync = SyncWrite(out.name, out if show else None)  # type: ignore[arg-type]
+            err_sync = SyncWrite(err.name, err if show else None, stderr_color)  # type: ignore[arg-type]
+
             with out_sync, err_sync:
                 instance = self.build_instance(request, self._option_class(env), out_sync, err_sync)
                 with instance as status:
