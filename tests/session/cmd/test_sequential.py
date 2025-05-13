c04567add7ce328e89d0e94eaebcae3f83d245e6
Robsdedude <rouven.bauer@neo4j.com>
2025-05-13 16:56:39 +0000

    Feat: include free_threaded flag in result-json (#3534)

--- tests/session/cmd/test_sequential.py
+++ tests/session/cmd/test_sequential.py
@@ -89,6 +89,7 @@ def test_result_json_sequential(
         "sysplatform": py_info.platform,
         "version": py_info.version,
         "version_info": list(py_info.version_info),
+        "free_threaded": py_info.free_threaded,
     }
     packaging_setup = get_cmd_exit_run_id(log_report, ".pkg", "setup")
     assert "result" not in log_report["testenvs"][".pkg"]
