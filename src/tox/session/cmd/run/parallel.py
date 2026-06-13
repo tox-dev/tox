2528e9357c9fefdecac2bb73512f77970ba21685
Bernát Gábor <gaborjbernat@gmail.com>
2026-06-13 08:04:30 -0700

    Drop obsolete ty: ignore directives

--- src/tox/session/cmd/run/parallel.py
+++ src/tox/session/cmd/run/parallel.py
@@ -65,7 +65,7 @@ def parallel_flags(
         type=parse_num_processes,
         default=default_parallel,
         metavar="VAL",
-        **({"nargs": "?"} if no_args else {}),  # ty: ignore[invalid-argument-type] # https://github.com/astral-sh/ty/issues/2586
+        **({"nargs": "?"} if no_args else {}),
     )
     our.add_argument(
         "-o",
